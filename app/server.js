3ae299ca969608a3ae8c35e328381d2ed000ca8e
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-10 19:12:27 +0430

    Add routes, controllers, and validations for authentication, projects, teams, and users

--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
+const { AllRoutes } = require('./router/router');
+
 module.exports = class Application {
     #express = require('express');
     #app = this.#express();
@@ -54,5 +56,6 @@ module.exports = class Application {
                 message: 'this is a new express application.'
             });
         });
+        this.#app.use(AllRoutes);
     }
 };
