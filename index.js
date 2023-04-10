a906ddcc58039d2826c49506720862fda32ea79d
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-10 14:27:19 +0430

    Add PORT variable to launch application with correct port

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const Application = require('./app/server');
+const PORT = process.env.PORT || 3000;
 const DB_URL = 'mongodb://127.0.0.1:27017/ProjectManagement';
-new Application(3000, DB_URL);
+new Application(PORT, DB_URL);
