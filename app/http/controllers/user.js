de48ac74fb4a029ae779409d54976c05374ed387
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-13 08:53:28 +0430

    Update UserController to add editProfile route and API request

--- app/http/controllers/user.js
+++ app/http/controllers/user.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/user");
+
 class UserController{
     getProfile(req, res, next) {
         try {
@@ -11,8 +13,31 @@ class UserController{
             next(error)
         }
     }
-    editProfile() {
-
+    async editProfile(req, res, next) {
+        try {
+            let data = req.body;
+            const userId = req.body._id;
+            let fields = ['first_name', 'last_name', 'skills'];
+            let badValue = ['', ' ', null, undefined, NaN, false, 0, [], {}];
+            Object.entries(data).forEach(([key, value]) => {
+                console.log(key, value);
+                if (!fields.includes(key)) delete data[key];
+                if (badValue.includes(value)) delete data[key];
+            });
+            console.log(data);
+            const result = await UserModel.updateOne({ _id: userId }, { $set: data });
+            if (result.modifiedCount > 0) {
+                return res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: 'Update profile successfully'
+                });
+            } else {
+                throw {status: 400, message: 'Update profile failed.'}
+            }
+        } catch (error) {
+            next(error);
+        }
     }
     addSkills() {
         
--- app/router/user.js
+++ app/router/user.js
@@ -1,8 +1,9 @@
-const {UserController} = require('../http/controllers/user');
+const { UserController } = require('../http/controllers/user');
 const { checkLogin } = require('../http/middlewares/autoLogin');
 const router = require('express').Router();
 
 router.get('/profile', checkLogin, UserController.getProfile);
+router.post('/profile', checkLogin, UserController.editProfile);
 
 module.exports = {
     userRoute: router
