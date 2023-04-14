5450f8815e6bc29fbf4c1624b18c110dfa079b28
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-14 06:12:05 +0430

    Update multer.js to support file size limit and validate file format

--- app/modules/multer.js
+++ app/modules/multer.js
@@ -8,11 +8,21 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const type = path.extname(file?.originalname || '');
-        cb(null, Date.now() + type);
+        const trueType = ['.png', '.jpg', '.jpeg', '.gif', '.webp'];
+        if (trueType.includes(type)) {
+            cb(null, Date.now() + type);
+        } else {
+            cb(new Error('File format not supported'));
+        }
     }
 });
 
-const multerUpload = multer({ storage });
+const multerUpload = multer({ 
+    storage,
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    } 
+});
 
 module.exports = {
     multerUpload
