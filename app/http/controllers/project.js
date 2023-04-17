c99f1d78cf6fcfd456340c17343bb0efb120e804
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-17 14:20:50 +0430

    Add tags to project controller and validator, and update project model to include tags and upload image

--- app/http/controllers/project.js
+++ app/http/controllers/project.js
@@ -3,9 +3,9 @@ const { ProjectModel } = require("../../models/project");
 class ProjectController{
     async createProject(req, res, next) {
         try {
-            const { title, text, image } = req.body;
+            const { title, text, image, tags } = req.body;
             const owner = req.user._id;
-            const result = await ProjectModel.create({title, text, owner, image });
+            const result = await ProjectModel.create({title, text, owner, image, tags });
             if (!result) throw {status: 400, message: 'Project not created.'};
             return res.status(201).json({
                 status: 201,
--- app/http/validations/project.js
+++ app/http/validations/project.js
@@ -3,6 +3,7 @@ const {body} = require('express-validator');
 function createProjectValidator() {
     return [
         body('title').notEmpty().withMessage('Title is required'),
+        body('tags').isArray({min: 0, max: 10}).withMessage('Tags must be an array with a maximum of 10 elements'),
         body('text').notEmpty().isLength({min: 20}).withMessage('Text is required and must be at least 20 characters long'),
     ]
 }
