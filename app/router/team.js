19f20cbb9f2dd6315780b3edb892a01297434b64
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-04-19 07:03:30 +0430

    Add functionality to create and get list of teams

--- app/router/team.js
+++ app/router/team.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const { TeamController } = require('../http/controllers/team');
+const { checkLogin } = require('../http/middlewares/autoLogin');
+const { expressValidatorMapper } = require('../http/middlewares/checkErrors');
+const { createTeamValidator } = require('../http/validations/team');
+
+router.post('/create', checkLogin, createTeamValidator(), expressValidatorMapper, TeamController.createTeam);
+router.get('/get-teams', checkLogin, expressValidatorMapper, TeamController.getListOfTeams);
 
 module.exports = {
     teamRoute: router
